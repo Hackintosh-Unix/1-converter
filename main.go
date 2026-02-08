80143b4c4dd4384d2b0e389e3696ca7b11c2682c
a.rybkin <a.rybkin@tbank.ru>
2026-02-08 07:22:56 +0300

    fix

--- main.go
+++ main.go
@@ -6,10 +6,9 @@ import "fmt"
 
 func main() {
 
-	var eur float64 = 1
 	const usdToEur float64 = 0.84
 	const usdToRub float64 = 77.05
-	var eurToRub float64 = eur * usdToRub / usdToEur
+	const eurToRub float64 = 1.0 * usdToRub / usdToEur
 	fmt.Println(eurToRub)
 
 }
