9f13026dcf1470bd1338af1a672f12e74d6ad12a
a.rybkin <a.rybkin@tbank.ru>
2026-02-10 19:55:44 +0300

    fix

--- main.go
+++ main.go
@@ -22,28 +22,46 @@ func userInput() (float64, string, string, error) {
 	var inputCur float64
 	var startCur string
 	var endCur string
-	var err error
 	fmt.Print("Введите исходную валюту (EUR/USD/RUB)")
 	fmt.Scan(&startCur)
-	if startCur != "EUR" || startCur != "USD" || startCur != "RUB" {
-		err = errors.New("Вы не правильно ввели имя валюты")
-	}
 	fmt.Print("Введите число")
 	fmt.Scan(&inputCur)
-	if inputCur < 0 {
-		err = errors.New("Неверное число")
-	}
 	fmt.Print("Введите целевую валюту (EUR/USD/RUB)")
 	fmt.Scan(&endCur)
-	if endCur != "EUR" || endCur != "USD" || endCur != "RUB" {
-		err = errors.New("Вы не правильно ввели имя валюты")
-	}
+	err := checkInput(inputCur, startCur, endCur)
 	return inputCur, startCur, endCur, err
 }
 
+func checkInput(inputCur float64, startCur string, endCur string) error {
+	var err error
+	if startCur != "EUR" && startCur != "USD" && startCur != "RUB" {
+		err = errors.New("Вы не правильно ввели имя валюты")
+	}
+	if endCur != "EUR" && endCur != "USD" && endCur != "RUB" {
+		err = errors.New("Вы не правильно ввели имя валюты")
+	}
+	if inputCur < 0 {
+		err = errors.New("Неверное число")
+	}
+	return err
+}
+
 func calculateCur(inputCur float64, startCur string, endCur string) float64 {
-	const usdToEur float64 = 0.84
-	const usdToRub float64 = 77.05
-	var eurToRub float64 = inputCur * usdToRub / usdToEur
-	return eurToRub
+	var result, course float64
+	switch {
+	case startCur == "USD" && endCur == "RUB":
+		course = 77.05
+	case startCur == "USD" && endCur == "EUR":
+		course = 0.84
+	case startCur == "EUR" && endCur == "USD":
+		course = 1.19
+	case startCur == "EUR" && endCur == "RUB":
+		course = 92.07
+	case startCur == "RUB" && endCur == "USD":
+		course = 0.013
+	case startCur == "RUB" && endCur == "EUR":
+		course = 0.011
+	}
+	result = inputCur * course
+	return result
 }
