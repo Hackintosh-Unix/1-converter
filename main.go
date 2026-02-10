988674b147ffda4d11fe908cf1d585d7947ad382
a.rybkin <a.rybkin@tbank.ru>
2026-02-10 19:34:02 +0300

    Домашнее задание 2

--- main.go
+++ main.go
@@ -1,21 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Домашнее задание 1-start
 
 func main() {
-
-	fmt.Println(calculateCur(userInput()))
-
+	for {
+		inputCur, startCur, endCur, err := userInput()
+		if err != nil {
+			fmt.Print(err)
+			continue
+		}
+		fmt.Println(calculateCur(inputCur, startCur, endCur))
+	}
 }
 
-func userInput() (float64, string, string) {
+func userInput() (float64, string, string, error) {
 	var inputCur float64
 	var startCur string
 	var endCur string
-	fmt.Scan(&inputCur, &startCur, &endCur)
-	return inputCur, startCur, endCur
+	var err error
+	fmt.Print("Введите исходную валюту (EUR/USD/RUB)")
+	fmt.Scan(&startCur)
+	if startCur != "EUR" || startCur != "USD" || startCur != "RUB" {
+		err = errors.New("Вы не правильно ввели имя валюты")
+	}
+	fmt.Print("Введите число")
+	fmt.Scan(&inputCur)
+	if inputCur < 0 {
+		err = errors.New("Неверное число")
+	}
+	fmt.Print("Введите целевую валюту (EUR/USD/RUB)")
+	fmt.Scan(&endCur)
+	if endCur != "EUR" || endCur != "USD" || endCur != "RUB" {
+		err = errors.New("Вы не правильно ввели имя валюты")
+	}
+	return inputCur, startCur, endCur, err
 }
 
 func calculateCur(inputCur float64, startCur string, endCur string) float64 {
